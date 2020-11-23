493c444ff1892b9b70e6bb2942186a2b3705a705
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-11-23 07:16:21 +0000

    check answers with state management

--- src/App.js
+++ src/App.js
@@ -2,14 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+function getValuesWithAnswer() {
+  const value1 = Math.floor(Math.random() * 100);
+  const value2 = Math.floor(Math.random() * 100);
+  const value3 = Math.floor(Math.random() * 100);
+  const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+  return {value1, value2, value3, proposedAnswer};
+}
 
 class App extends Component {
+  state = {
+    value1: null,
+    value2: null,
+    value3: null,
+    proposedAnswer: null,
+    numQuestions: 0,
+    numCorrect: 0
+  }
+
+  componentDidMount() {
+    const {value1, value2, value3, proposedAnswer} = getValuesWithAnswer();
+	this.setState({value1, value2, value3, proposedAnswer});
+  }
+
+  onTrue = () => {
+    const checkAnswer = this.state.value1 + this.state.value2 + this.state.value3;
+    if (checkAnswer === this.state.proposedAnswer) {
+      this.setState({numCorrect: this.state.numCorrect + 1});
+    }
+    const {value1, value2, value3, proposedAnswer} = getValuesWithAnswer();
+    
+    this.setState({value1, value2, value3, proposedAnswer, numQuestions: this.state.numQuestions + 1});
+  }
+
+  onFalse = () => {
+    const checkAnswer = this.state.value1 + this.state.value2 + this.state.value3;
+    if (checkAnswer !== this.state.proposedAnswer) {
+      this.setState({numCorrect: this.state.numCorrect + 1});
+    }
+    const {value1, value2, value3, proposedAnswer} = getValuesWithAnswer();
+    
+    this.setState({value1, value2, value3, proposedAnswer, numQuestions: this.state.numQuestions + 1});
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,12 +55,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+          <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={this.onTrue}>True</button>
+          <button onClick={this.onFalse}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
